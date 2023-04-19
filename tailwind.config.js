e1bf6f43cabef59a0ee3ef661f675cc01f8fd4e9
Leo Ji <leo@leoji.codes>
2023-04-18 20:48:37 -0500

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
+    fontFamily: {
+      mistrully: ["Mistrully", "sans-serif"],
+      montserrat: ["Montserrat", "sans-serif"],
+      "futura-pt": ["Futura PT", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
