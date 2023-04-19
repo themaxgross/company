27d631317836b4fc156d94ec3f9794b2a1eb19f7
Leo Ji <leo@leoji.codes>
2023-04-18 21:18:42 -0500

    animate-fade-in

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,17 +10,19 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
-const Index = () => (
-  <div className="w-screen h-screen bg-brand-primary text-white p-4">
-    <div className="w-full h-full flex items-center justify-center">
-      <div>
-        <h1 className="text-4xl font-semibold mb-2">Meow!</h1>
-        <h2 className="text-2xl mb-12">
-          Coming soon from a cat roommate near you.
-        </h2>
-        <p className="text-lg font-extralight">– Leo Ji LLC</p>
+const Index = () => {
+  return (
+    <div className="w-screen h-screen bg-brand-primary text-white p-4">
+      <div className="w-full h-full flex items-center justify-center animate-fade-in">
+        <div>
+          <h1 className="text-4xl font-semibold mb-2">Meow!</h1>
+          <h2 className="text-2xl mb-12">
+            Coming soon from a cat roommate near you.
+          </h2>
+          <p className="text-lg font-extralight">– Leo Ji LLC</p>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 export default Index;
