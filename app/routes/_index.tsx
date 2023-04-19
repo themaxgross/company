b711e28a18163dad68ca22ff092cb6d5e76089ff
Leo Ji <leo@leoji.codes>
2023-04-18 21:14:03 -0500

    add fonts; add intro page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,37 +1,26 @@
 import type { V2_MetaFunction } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: "New Remix App" }];
+  return [
+    { title: "Leo Ji LLC" },
+    {
+      name: "description",
+      content: "Coming soon from a cat roommate near you.",
+    },
+  ];
 };
 
 const Index = () => (
-  <div className="">
-    <h1>Welcome to Remix</h1>
-    <ul>
-      <li>
-        <a
-          target="_blank"
-          href="https://remix.run/tutorials/blog"
-          rel="noreferrer"
-        >
-          15m Quickstart Blog Tutorial
-        </a>
-      </li>
-      <li>
-        <a
-          target="_blank"
-          href="https://remix.run/tutorials/jokes"
-          rel="noreferrer"
-        >
-          Deep Dive Jokes App Tutorial
-        </a>
-      </li>
-      <li>
-        <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-          Remix Docs
-        </a>
-      </li>
-    </ul>
+  <div className="w-screen h-screen bg-brand-primary text-white p-4">
+    <div className="w-full h-full flex items-center justify-center">
+      <div>
+        <h1 className="text-4xl font-semibold mb-2">Meow!</h1>
+        <h2 className="text-2xl mb-12">
+          Coming soon from a cat roommate near you.
+        </h2>
+        <p className="text-lg font-extralight">– Leo Ji LLC</p>
+      </div>
+    </div>
   </div>
 );
 export default Index;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,27 @@ module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
     fontFamily: {
-      mistrully: ["Mistrully", "sans-serif"],
-      montserrat: ["Montserrat", "sans-serif"],
-      "futura-pt": ["Futura PT", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+      raleway: ["Raleway", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        "brand-primary": {
+          DEFAULT: "#0E1C38",
+          50: "#FAFBFE",
+          100: "#E5ECF9",
+          200: "#BDCDEE",
+          300: "#94AFE4",
+          400: "#6B90DA",
+          500: "#4271D0",
+          600: "#2D59B2",
+          700: "#22458A",
+          800: "#183061",
+          900: "#0E1C38",
+          950: "#091224",
+        },
+      },
+    },
   },
   plugins: [],
 };
