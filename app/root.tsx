b0daa808622ce9b3cd7481bcb54b341e5cffd4ef
Leo Ji <leo@leoji.codes>
2023-04-18 20:41:00 -0500

    Initial commit

---
+++ app/root.tsx
@@ -0,0 +1,38 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import type { LinksFunction } from "@remix-run/server-runtime";
+
+import styles from "./tailwind.css";
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: styles },
+    { rel: "icon", href: "/favicon.ico" },
+  ];
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
