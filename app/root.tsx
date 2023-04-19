430379b6ac57f15174a86b0c0b15258d4a07bcbb
Leo Ji <leo@leoji.codes>
2023-04-18 21:25:34 -0500

    add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -66,6 +66,24 @@ const App = () => (
       <meta name="viewport" content="width=device-width,initial-scale=1" />
       <Meta />
       <Links />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
     </head>
     <body className="font-raleway">
       <Outlet />
