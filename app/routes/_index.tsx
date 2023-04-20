b3ff60e846634d6130c4eaf4e8e832ca04a56a70
Leo Ji <leo@leoji.codes>
2023-04-19 22:03:03 -0500

    updated styling

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -18,21 +18,17 @@ export const meta: V2_MetaFunction = () => {
 export const Index = () => {
   return (
     <div className="bg-brand-primary p-8 min-w-screen min-h-screen flex justify-stretch items-stretch">
-      <div className="p-5 bg-gray-200 text-slate-800 flex flex-row font-raleway m-0  w-full">
-        <div className="px-5 md:px-12 w-1/2 mr-auto ml-0 min-h-24 flex flex-col justify-around">
+      <div className="bg-gray-200 text-slate-800 flex flex-row font-raleway w-full">
+        <div className="py-12 px-4 md:px-12 w-full xl:w-1/2 md:mr-auto min-h-24 flex flex-col justify-around">
           <div>
             <Fade cascade={true} triggerOnce={true} duration={2000}>
-              <h1 className="text-6xl font-semibold mb-6 md:mb-12 leading-tight">
-                Websites so fast,
-                <br />
-                they'll make you meow.
+              <h1 className="text-4xl md:text-5xl xl:text-6xl font-semibold mb-6 md:mb-12 leading-tight">
+                Websites so fast, they'll make you meow.
               </h1>
-              <h1 className="text-6xl font-semibold mb-6 md:mb-12 leading-tight">
-                Designs so beautiful,
-                <br />
-                they'll make you purr.
+              <h1 className="text-4xl md:text-5xl xl:text-6xl font-semibold mb-6 md:mb-12 leading-tight">
+                Designs so beautiful, they'll make you purr.
               </h1>
-              <h2 className="text-3xl mb-12">
+              <h2 className="text-xl md:text-2xl xl:text-3xl mb-6 md:mb-12">
                 Cats not included. Coming soon from a cat roommate near you.
               </h2>
               <p className="text-lg font-extralight">– Leo Ji LLC</p>
