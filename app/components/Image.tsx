64e2b2f9bd7df0afb8a7ad1f1ba3430cc91ae5f4
Leo Ji <leo@leoji.codes>
2023-04-21 23:30:20 -0500

    update image component to work better with cloudflare pages, include a domain in src urls to work with cloudflare image resizing

--- app/components/Image.tsx
+++ app/components/Image.tsx
@@ -5,15 +5,25 @@ type ImageProps = RemixImageProps & {
   loader?: never;
   loaderUrl?: never;
 };
+// hardcode the domain for now
+const domain = "https://leoji.company";
 
-export default function Image({ responsive, ...props }: ImageProps) {
+export default function Image({ responsive, src, ...props }: ImageProps) {
   const isDev = process.env.NODE_ENV === "development";
+  const useCloudflare = !isDev;
+
+  // If src does not start with a domain, then it is a local image
+  // and we need to add the domain to the src
+  if (useCloudflare && src && !src.startsWith("http")) {
+    src = `${domain}${src}`;
+  }
 
   return (
     <RemixImage
       {...props}
+      src={src}
       loader={cloudflareImagesLoader}
-      responsive={isDev ? undefined : responsive}
+      responsive={useCloudflare ? responsive : undefined}
     />
   );
 }
