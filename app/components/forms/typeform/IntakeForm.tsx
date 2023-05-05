42340342fcb9bcb85f0031e7f58ae045517afee2
Leo Ji <leo@leoji.codes>
2023-05-05 11:19:47 -0500

    lazy-load the intake typeform

--- app/components/forms/typeform/IntakeForm.tsx
+++ app/components/forms/typeform/IntakeForm.tsx
@@ -1,11 +1,19 @@
 import { Widget } from "@typeform/embed-react";
+import { useHydrated } from "remix-utils";
 
 const FORM_ID = "iQA1VPEB";
 
 export default function TypeformIntakeForm() {
+  const isHydrated = useHydrated();
+
   return (
     <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-16 md:py-20">
-      <Widget id={FORM_ID} className="w-full h-full" height={720} />
+      <Widget
+        id={FORM_ID}
+        className="w-full h-full"
+        height={720}
+        lazy={isHydrated}
+      />
     </div>
   );
 }
