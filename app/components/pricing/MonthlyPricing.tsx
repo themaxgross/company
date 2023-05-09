6e18f261bed3805d3aafec03290c49032ca13841
Leo Ji <leo@leoji.codes>
2023-05-08 21:32:56 -0500

    update MonthlyPricing.tsx

--- app/components/pricing/MonthlyPricing.tsx
+++ app/components/pricing/MonthlyPricing.tsx
@@ -2,19 +2,26 @@ export function MonthlyPricing({ priceMonthly }: { priceMonthly?: string }) {
   return (
     <p className="mt-6 flex items-baseline gap-x-1">
       {priceMonthly ? (
-        <>
-          <span className="text-sm font-semibold leading-6 text-gray-600 dark:text-brand-white">
-            Starting at{" "}
-          </span>
-          <span className="text-4xl font-bold tracking-tight text-gray-900 dark:text-brand-white">
-            {priceMonthly}
-          </span>
-          <span className="text-sm font-semibold leading-6 text-gray-600 dark:text-brand-white">
-            /month
-          </span>
-        </>
+        <div className="flex flex-col gap-y-1">
+          <div>
+            <span className="text-sm font-semibold leading-6 text-gray-600 dark:text-brand-white">
+              Est.{" "}
+            </span>
+            <span className="text-4xl font-bold tracking-tight text-gray-900 dark:text-brand-white">
+              {priceMonthly}
+            </span>
+            <span className="text-sm font-semibold leading-6 text-gray-600 dark:text-brand-white">
+              /mo.
+            </span>
+          </div>
+          <div>
+            <span className="text-xs font-futura font-lighter leading-3 text-gray-600 dark:text-slate-200">
+              Actual price varies based on features and contract negotiation.
+            </span>
+          </div>
+        </div>
       ) : (
-        <span className="text-sm font-semibold leading-6 text-gray-600 dark:text-slate-200">
+        <span className="leading-6 text-gray-600 dark:text-slate-200 text-xl small-caps font-medium font-futura">
           Contact for pricing
         </span>
       )}
